01af7bbf5b1c265ae203b4d99e814ef45c0ea99c
Giosuè Santoro <giosuesantoro@hotmail.it>
2021-01-13 16:21:48 +0100

    pulsanti sistemati

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
 
-    $('next').click(function() {
-        $('.active').removeClass('active').next().addClass('active')
-    })
-
-    $('prev').click(function() {
+    //Funzione per rendere attivo bottone prev
+    $('.prev').click(function() {
         $('.active').removeClass('active').prev().addClass('active')
     })
 
+    //Funzione per rendere attivo bottone next
+    $('.next').click(function() {
+        $('.active').removeClass('active').next().addClass('active')
+    })
+
 })
+
+
 
 
 
