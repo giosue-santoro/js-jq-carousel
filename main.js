4148c523784b8ce048343d4cc2f6b30347f1cad8
Giosuè Santoro <giosuesantoro@hotmail.it>
2021-01-13 17:25:13 +0100

    esercizio completo

--- main.js
+++ main.js
@@ -1,19 +1,42 @@
 $(document).ready(function() {
+    
+    //Funzione per rendere attivo bottone next
+    $('.next').click(function(){
+        //Condizioni per rendere attivo bottone next anche dopo prima/ultima immagine
+        if ($('.last').hasClass('active') === false) {
+            $('.active').removeClass('active').next().addClass('active')
+        } else {
+            $('.active').removeClass('active')
+            $('.first').addClass('active')
+        }
+    })
 
     //Funzione per rendere attivo bottone prev
-    $('.prev').click(function() {
-        $('.active').removeClass('active').prev().addClass('active')
+    $('.prev').click(function(){
+        //Condizioni per rendere attivo bottone prev anche dopo prima/ultima immagine
+        if ($('.first').hasClass('active') === false) {
+            $('.active').removeClass('active').prev().addClass('active')
+        } else {
+            $('.active').removeClass('active')
+            $('.last').addClass('active')
+        }
     })
+    
+    //Ridimensionamento immagini
+    $('img').css({'width':'1200px'})
+    
+})
+        
+
+    //Funzione per rendere attivo bottone prev
+    // $('.prev').click(function() {
+    //     $('.active').removeClass('active').prev().addClass('active')
+    // })
 
     //Funzione per rendere attivo bottone next
-    $('.next').click(function() {
-        $('.active').removeClass('active').next().addClass('active')
-    })
-
-})
-
-
-
+    // $('.next').click(function() {
+    //     $('.active').removeClass('active').next().addClass('active')
+    // })
 
 
 
